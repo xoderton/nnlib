3f545d82eb0c8722ae31a14eb512903739e3435f
shockpast <shockpast@gmail.com>
2024-07-21 00:12:06 +0300

    feat: added player.Find function, mostly from easylua

--- lua/nnlib/player.lua
+++ lua/nnlib/player.lua
@@ -47,3 +47,50 @@ function player.FindByUserGroup(usergroup)
 
   return result, #result
 end
+
+---Finds [x] player using provided `[steamid/steamid64/entindex/name/ip]`
+---@param info number|string
+---@return Player|Entity
+function player.Find(info)
+  if info == nil then return NULL end
+
+  local str = tostring(info)
+  local num = tostring(info)
+
+  -- SteamID
+  if string.find(str, "STEAM") then
+    for _, v in player.Iterator() do
+      if v:SteamID() ~= str then continue end
+      return v
+    end
+  end
+
+  -- EntIndex
+  if num ~= nil then
+    ---@diagnostic disable-next-line: param-type-mismatch
+    local ply = Entity(num)
+    if ply:IsValid() then return ply end
+  end
+
+  -- SteamID64
+  if utf8.len(str) == 17 and str[1] == "7" then
+    local ply = player.GetBySteamID64(str)
+    if ply ~= nil and ply:IsValid() then return ply end
+  end
+
+  -- IP Address
+  if SERVER and string.find(str, "%d+%.%d+%.%d+%.%d+") then
+    for _, v in player.Iterator() do
+      if string.find(v:IPAddress(), str) == nil then continue end
+      return v
+    end
+  end
+
+  -- Exact Search by Name
+  for _, v in player.Iterator() do
+    if v:Name() ~= str then continue end
+    return v
+  end
+
+  return NULL
+end
